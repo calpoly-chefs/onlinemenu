9ff2344aaa290f245d96bad66a0b615154f0a151
Will Fuchs <wfuchs@calpoly.edu>
2019-10-23 10:46:30 -0700

    switch to prod build

--- app/main/config.py
+++ app/main/config.py
@@ -11,10 +11,8 @@ class Config:
 
 
 class DevelopmentConfig(Config):
-    # uncomment the line below to use postgres
     SQLALCHEMY_DATABASE_URI = postgres_local_base
     DEBUG = True
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_main.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
@@ -28,8 +26,7 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    # uncomment the line below to use postgres
-    # SQLALCHEMY_DATABASE_URI = postgres_local_base
+    SQLALCHEMY_DATABASE_URI = postgres_local_base
 
 
 config_by_name = dict(
